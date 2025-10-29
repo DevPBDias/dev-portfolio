d42b91bb088f920c80a35cd60175dd9367a75985
DevPBDias <devpbdias@gmail.com>
2025-10-29 12:04:26 -0300

    fix: delete unused components fot the app

--- src/_components/layout/DarkOverlay.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DarkOverlay = () => {
-  return (
-    <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/40 to-black/70 -z-10" />
-  );
-};
-
-export default DarkOverlay;
